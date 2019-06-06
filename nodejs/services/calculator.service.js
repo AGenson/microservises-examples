f20744579f4b325bca19919e527e433014a0487e
Anthony Genson <anthony.genson@isen.yncrea.fr>
2019-06-06 20:45:00 +0200

    Added error logger for Calculator

--- nodejs/services/calculator.service.js
+++ nodejs/services/calculator.service.js
@@ -3,6 +3,10 @@ const bodyParser = require('body-parser')
 
 const { CalculatorPort } = require('../locations')
 
+const throwErr = (res, err) => {
+	console.log(`Error: ${err.type}`)
+	res.status(err.code).json(err)
+}
 const operations = {
 	'+': ({ x, y }) => x + y,
 	'-': ({ x, y }) => x - y,
@@ -19,18 +23,18 @@ app.use((req, res, next) => {
 
 	let { operator, values } = req.body
 
-	if (typeof operator == 'undefined')
-		res.status(code).json({ type: 'MISSING_INPUT_ERROR', code, message: "Missing operator: please use one of the following operators.", data: operators_arr })
+	if (typeof operator != 'string')
+		throwErr(res, { type: 'MISSING_INPUT_ERROR', code, message: "Missing operator: please use one of the following operators.", data: operators_arr })
 	else if (!operators_arr.includes(operator))
-		res.status(code).json({ type: 'INVALID_INPUT_ERROR', code, message: "Invalid operator: please use one of the following operators.", data: operators_arr })
-	else if (typeof values == 'undefined')
-		res.status(code).json({ type: 'MISSING_INPUT_ERROR', code, message: "Missing input: please specify the field 'values'." })
-	else if (typeof values.x == 'undefined')
-		res.status(code).json({ type: 'MISSING_INPUT_ERROR', code, message: "Missing input: please specify the field 'values.x'." })
-	else if (typeof values.y == 'undefined')
-		res.status(code).json({ type: 'MISSING_INPUT_ERROR', code, message: "Missing input: please specify the field 'values.y'." })
+		throwErr(res, { type: 'INVALID_INPUT_ERROR', code, message: "Invalid operator: please use one of the following operators.", data: operators_arr })
+	else if (typeof values != 'object')
+		throwErr(res, { type: 'MISSING_INPUT_ERROR', code, message: "Missing input: please specify the field 'values'." })
+	else if (typeof values.x != 'number')
+		throwErr(res, { type: 'MISSING_INPUT_ERROR', code, message: "Missing input: please specify the field 'values.x'." })
+	else if (typeof values.y != 'number')
+		throwErr(res, { type: 'MISSING_INPUT_ERROR', code, message: "Missing input: please specify the field 'values.y'." })
 	else if (operator == '/' && values.y == 0)
-		res.status(code).json({ type: 'ZERO_DIVISION_ERROR', code, message: "Zero Division Error: tried to divide a number by 0." })
+		throwErr(res, { type: 'ZERO_DIVISION_ERROR', code, message: "Zero Division Error: tried to divide a number by 0." })
 	else
 		next()
 })
