6df965e64ecc9afe65a3cd2a37355c3817753ae4
Anthony Genson <anthony.genson@isen.yncrea.fr>
2019-06-06 23:16:51 +0200

    Created Authenticated Calculator Service

---
+++ nodejs/authenticated/calculator/proxy.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const axios = require('axios')
+
+const { ProxyPort, AuthentificatorPort, CalculatorPort } = require('../../locations')
+const authorize = require('./auth.middleware')(`http://localhost:${AuthentificatorPort}`)
+
+const throwErr = (res, err) => {
+	console.log(`Error: ${err.type}`)
+	res.status(err.code).json(err)
+}
+
+const authorisePaths = [ '/calculator' ]
+
+let app = express()
+app.use(bodyParser.json())
+authorisePaths.forEach( (path) => app.use(path, authorize) )
+
+app.post('/calculator', async (req, res) => {
+    axios({
+        method: 'post',
+        baseURL: `http://localhost:${CalculatorPort}`,
+        url: req.path,
+        data: req.body
+    }).then( response => { res.json({ ...(response.data), key_info: req.body.auth }) })
+    .catch( err => { throwErr(res, err.response.data) })
+})
+
+app.listen( ProxyPort, () => { console.log(`Authentificator service started.\nEndpoint: http://localhost:${ProxyPort}\n`) })
--- nodejs/locations.js
+++ nodejs/locations.js
@@ -1,6 +1,7 @@
 // Ports of the different locations, to add to http://localhost:PORT
 
 module.exports = {
+    ProxyPort: '8000',
     CalculatorPort: '8001',
     AuthentificatorPort: '8002'
 }
